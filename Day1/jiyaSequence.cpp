887e1359fa46fdc904bec6ecde29b74e77610209
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-01 18:08:08 +0500

    problem 2

--- Day1/jiyaSequence.cpp
+++ Day1/jiyaSequence.cpp
@@ -3,16 +3,45 @@
 //
 #include <iostream>
 #include <bits/stdc++.h>
+#include <string>
+
 using namespace std;
 
-int main(){
-int test;
-int n;
-string num;
-cin >> test;
-cin >> n;
+int main() {
+    int test;
+    int n, j = 0;
+    int num[15];
+    char input[30];
+    int output = 1;
+    cin >> test;
 
+    while (test) {
+        cin >> n;
+        getchar();
+        cin.getline(input, (n * 2));
 
+        for (int i = 0; i < 15; ++i) {
+            num[i] = 1;
+        }
+
+        for (int i = 0; i < (n * 2) - 1; ++i) {
+            if (input[i] != ' ') {
+                num[j] = input[i] - '0';
+                j++;
+            }
+        }
+
+        for (int i = 0; i < 15; ++i) {
+            output *= num[i];
+        }
+
+        if (output % 10 == 2 || output % 10 == 3 || output % 10 == 5) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+        test--;
+    }
 
     return 0;
 }
