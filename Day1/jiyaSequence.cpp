4747c5da3a73a4e519b34e560607ba8fcc002efc
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-01 14:19:17 +0500

    problem 2

---
+++ Day1/jiyaSequence.cpp
@@ -0,0 +1,18 @@
+//
+// Created by alam on 01/07/22.
+//
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+int test;
+int n;
+string num;
+cin >> test;
+cin >> n;
+
+
+
+    return 0;
+}
