c8cbac1475a0023d1729cfd581d8592c226c3706
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-01 22:05:36 +0500

    problem 2

--- Day1/jiyaSequence.cpp
+++ Day1/jiyaSequence.cpp
@@ -7,42 +7,77 @@
 
 using namespace std;
 
-int main() {
-    int test;
+class test {
     int n;
     int num[15];
-    char input[30];
-    int output = 1;
-    cin >> test;
+    int output;
+public:
 
-    while (test) {
-        int j = 0;
-        cin >> n;
-        getchar();
-        cin.getline(input, (n * 2));
-
+    test() {
+        n = 1;
+        output = 0;
         for (int i = 0; i < 15; ++i) {
             num[i] = 1;
         }
+    }
 
+    void setN(int a) {
+        n = a;
+    }
+
+    void setNum(char *c) {
+        int j = 0;
+        for (int i = 0; i < 15; ++i) {
+            num[i] = 1;
+        }
         for (int i = 0; i < (n * 2) - 1; ++i) {
-            if (input[i] != ' ') {
-                num[j] = input[i] - '0';
+            if (c[i] != ' ') {
+                num[j] = c[i] - '0';
                 j++;
             }
         }
+    }
 
+    void getAns() {
+        output = 1;
         for (int i = 0; i < 15; ++i) {
             output *= num[i];
         }
-
         if (output % 10 == 2 || output % 10 == 3 || output % 10 == 5) {
             cout << "YES" << endl;
         } else {
             cout << "NO" << endl;
         }
-        test--;
     }
+};
+
+
+int main() {
+
+    int t, d, i = 0;
+    char f[30];
+    test *a;
+    cin >> t;
+    a = new test[t];
+    int temp = t;
+    while (t > 0) {
+        cin >> d;
+        getchar();
+        cin.getline(f, 30);
+        a[i].setN(d);
+        a[i].setNum(f);
+        t--;
+        i++;
+    }
+
+    i = 0;
+
+    while (temp > 0) {
+        a[i].getAns();
+        i++;
+        temp--;
+    }
+
 
     return 0;
 }
