bc6204c8f32ab020e7620ad8d072900257de21b5
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-03 00:09:41 +0500

    problem 1, Day 2

--- Day2/wave.cpp
+++ Day2/wave.cpp
@@ -1,6 +1,35 @@
 ////
 //// Created by alam on 02/07/22.
 ////
+
+//so this works, but I don't know why the fuck it is duplicating the output on GeeksforGeeks.
+//class Solution{
+//public:
+//    // arr: input array
+//    // n: size of array
+//    //Function to sort the array into a wave-like array.
+//    void convertToWave(int n, vector<int>& arr){
+//        int temp;
+//        for (int i = 0; i < n; ++i) {
+//            if (i != n-1) {
+//                temp = arr[i];
+//                arr[i] = arr[i + 1];
+//                arr[i + 1] = temp;
+//                i++;
+//            }
+//        }
+//
+//        for(int i=0; i < arr.size(); i++){
+//            cout << arr.at(i) << ' ';
+//        }
+//    }
+//
+//};
+
+
+
+
+
 #include <iostream>
 using namespace std;
 
