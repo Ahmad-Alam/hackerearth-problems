1880b3f40c4dc745e66c945993e18811d22d4981
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-02 19:57:54 +0500

    problem 1, Day 2

--- Day2/divby3.cpp
+++ Day2/divby3.cpp
@@ -1,9 +1,20 @@
 //
 // Created by alam on 02/07/22.
 //
+#include <iostream>
+#include <bits/stdc++.h>
+
+
 class Solution {
 public:
     int isPossible(int N, int arr[]) {
-        // code here
+        int sum = 0;
+        for (int i = 0; i < N; ++i) {
+            sum += arr[i];
+            if(sum%3 == 0){
+                return 1;
+            }
+            return 0;
+        }
     }
 };
