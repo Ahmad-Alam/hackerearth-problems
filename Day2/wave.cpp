8d7997ec4ca51ce4d4b3cd82b9f7491fc2b5cc5d
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-03 00:06:07 +0500

    problem 1, Day 2

--- Day2/wave.cpp
+++ Day2/wave.cpp
@@ -1,5 +1,25 @@
-//
-// Created by alam on 02/07/22.
-//
+////
+//// Created by alam on 02/07/22.
+////
 #include <iostream>
 using namespace std;
+
+
+int main() {
+    int n;
+    int arr[6] = {2, 4, 7, 8, 9, 10};
+    int temp;
+    for (int i = 0; i < 6; ++i) {
+        if (i != 5) {
+            temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+            i++;
+        }
+    }
+    for (int i = 0; i < 6; ++i) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
