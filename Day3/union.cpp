6537e0e6cf8b1977e0efdcf1b63ebe74e4a770d5
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-04 11:45:34 +0500

    problem 2, Day 3

--- Day3/union.cpp
+++ Day3/union.cpp
@@ -9,10 +9,13 @@ using namespace std;
 int main(){
     int* a;
     int* b;
+    int* c;
+    int* d;
     int aSize;
     int bSize;
     cin >> aSize;
     cin >> bSize;
+    int cSize = aSize + bSize;
     a = new int[aSize];
     b = new int[bSize];
     for (int i = 0; i < aSize; ++i) {
@@ -21,8 +24,30 @@ int main(){
     for (int i = 0; i < bSize; ++i) {
         cin >> b[i];
     }
+    for (int i = 0; i < aSize; ++i) {
+        for (int j = 0; j < bSize; ++j) {
+            if(a[i] == b[j]){
+                if (b[j] != 99){
+                    cSize--;
+                }
+                b[j] = 99;
 
+            }
+        }
+    }
 
+    c = new int[cSize];
+    d = new int[aSize+bSize];
 
+    for (int i = 0; i < aSize+bSize; ++i) {
+        if(i > aSize){
+            d[i] = b[i-bSize];
+        }
+        d[i] = a[i];
+    }
+
+    for (int i = 0; i < aSize+bSize; ++i) {
+        cout << d[i] << " ";
+    }
     return 0;
 }
