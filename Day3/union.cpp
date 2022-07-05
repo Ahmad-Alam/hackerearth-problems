6cbfc35eb98d0bb1be339504cc0c80db23ab5a94
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-05 10:55:50 +0500

    problem 2, Day 3

--- Day3/union.cpp
+++ Day3/union.cpp
@@ -1,53 +1,28 @@
 //
 // Created by alam on 03/07/22.
 //
-//show a union of two arrays!
+//count of elements in the union of two arrays!
 
 #include <iostream>
+#include <set>
 using namespace std;
 
+int doUnion(int a[], int n, int b[], int m)  {
+    set<int> ans;
+    for(int i=0;i<n;i++){
+        ans.insert(a[i]);
+    }
+
+    for(int i=0;i<m;i++){
+        ans.insert(b[i]);
+    }
+
+    return ans.size();
+}
+
+
 int main(){
-    int* a;
-    int* b;
-    int* c;
-    int* d;
-    int aSize;
-    int bSize;
-    cin >> aSize;
-    cin >> bSize;
-    int cSize = aSize + bSize;
-    a = new int[aSize];
-    b = new int[bSize];
-    for (int i = 0; i < aSize; ++i) {
-        cin >> a[i];
-    }
-    for (int i = 0; i < bSize; ++i) {
-        cin >> b[i];
-    }
-    for (int i = 0; i < aSize; ++i) {
-        for (int j = 0; j < bSize; ++j) {
-            if(a[i] == b[j]){
-                if (b[j] != 99){
-                    cSize--;
-                }
-                b[j] = 99;
 
-            }
-        }
-    }
 
-    c = new int[cSize];
-    d = new int[aSize+bSize];
-
-    for (int i = 0; i < aSize+bSize; ++i) {
-        if(i > aSize){
-            d[i] = b[i-bSize];
-        }
-        d[i] = a[i];
-    }
-
-    for (int i = 0; i < aSize+bSize; ++i) {
-        cout << d[i] << " ";
-    }
     return 0;
 }
