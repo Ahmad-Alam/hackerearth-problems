93baa8a00097708836d49b1de08557191b53cdde
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-05 23:33:58 +0500

    problem 3, Day 3

---
+++ Day3/findDupe.cpp
@@ -0,0 +1,20 @@
+//
+// Created by alam on 05/07/22.
+//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findDuplicate(vector<int>& nums){
+    for (int i = 0; i < nums.size(); ++i) {
+        
+    }
+}
+
+
+int main(){
+    
+    
+    
+    return 0;
+}
