d326a79c9e13cd6a8a124ce640668f304f2f3dc7
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-05 23:57:56 +0500

    problem 3, Day 3

--- Day3/findDupe.cpp
+++ Day3/findDupe.cpp
@@ -7,13 +7,18 @@ using namespace std;
 
 int findDuplicate(vector<int>& nums){
     for (int i = 0; i < nums.size(); ++i) {
-        
+        for (int j = i+1; j < nums.size(); ++j) {
+            if(nums[i] == nums[j]){
+                return nums[i];
+            }
+        }
     }
 }
 
 
 int main(){
-    
+    vector<int> num = {3,1,3,4,2};
+    cout << findDuplicate(num);
     
     
     return 0;
