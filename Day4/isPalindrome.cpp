6fc95b6ea77b2c02d61cb32be557b8a1fd0b0907
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-06 10:16:58 +0500

    problem 1, Day 4

--- Day4/isPalindrome.cpp
+++ Day4/isPalindrome.cpp
@@ -1,6 +1,18 @@
 //
 // Created by alam on 06/07/22.
-//
+//check if a number is a palindrome or not
+
 #include <iostream>
 #include <string>
 using namespace std;
+
+int isPalindrome(char S[]){
+
+}
+
+int main(){
+
+
+
+    return 0;
+}
