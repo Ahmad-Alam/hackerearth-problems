3ffa3a0a69d1ff667fa10047acbfd7781321a48d
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-06 10:15:55 +0500

    problem 1, Day 4

---
+++ Day4/isPalindrome.cpp
@@ -0,0 +1,6 @@
+//
+// Created by alam on 06/07/22.
+//
+#include <iostream>
+#include <string>
+using namespace std;
---
+++ Day4/reverse.cpp
@@ -0,0 +1,27 @@
+//
+// Created by alam on 06/07/22.
+//reverse a string
+//push everything in a stack and then pop it
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+void reverseString(vector<char>& s){
+    vector<char> temp;
+    int size = s.size();
+    for (int i = size-1; i >= 0; --i) {
+        temp.push_back(s[i]);
+    }
+    for (int i = 0; i < temp.size(); ++i) {
+        cout << temp[i] << " ";
+    }
+}
+
+int main(){
+    vector<char> some = {'a', 'h', 'm', 'a', 'd'};
+    reverseString(some);
+
+
+    return 0;
+}
