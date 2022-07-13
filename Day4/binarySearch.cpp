4a7d517a8311817f73b68c82037985003a8597cb
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-13 13:26:34 +0500

    starting

--- Day4/binarySearch.cpp
+++ Day4/binarySearch.cpp
@@ -1,24 +1,28 @@
-//practicing writing code efficiently using neovim
-//this program searches for an integer, if it is present in the array return the
-//index of the element, otherwise return -1.
+// practicing writing code efficiently using neovim
+// this program searches for an integer, if it is present in the array return
+// the index of the element, otherwise return -1.
 
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int size){
+int binarySearch(int arr[], int size, int t) {
+    if (arr[size / 2] == t) {
+        return size/2;
+    }
+    else{
 
+    }
 }
 
-
-int main(){
-int n, t;
-int* arr;
-cin >> n;
-arr = new int[n];
-for(int i = 0; i < n; i++){
-    cin >> arr[i];
-}
-cin >> t;
+int main() {
+    int n, t;
+    int* arr;
+    cin >> n;
+    arr = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cin >> t;
 
     return 0;
 }
