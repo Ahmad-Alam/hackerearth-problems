3b5e5221393d7cd16e2b3695914936b4f4ff7307
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-19 12:04:09 +0500

    day 5

--- Day5/peak.cpp
+++ Day5/peak.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+int findPeak(vector<int> &arr) {
+    // Write your code here
+    int s =0;
+    int e = arr.size()-1;
+    int mid = s+(e-s)/2;
+    
+    while(s<e){
+        if(arr[mid] < arr[mid+1]){
+            s=mid+1;
+        }else{
+            e=mid;
+        }
+        mid = s+(e-s)/2;
+    }
+    return arr[s];
+}
 
 int main(){
 
-   int* arr;
-   
+   vector<int> arr = {4, 5, 7, 2, 9, 0};
+   cout << findPeak(arr) << endl;
 
 
 
