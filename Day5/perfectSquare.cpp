5684680f111e1701bf4b3985708c9813a2b8b42d
Ahmad-Alam <ahmad22july@gmail.com>
2022-07-19 18:43:28 +0500

    day 5

--- Day5/perfectSquare.cpp
+++ Day5/perfectSquare.cpp
@@ -2,14 +2,22 @@
 // Created by alam on 19/07/22.
 //
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-bool isPerfectSquare(long long n){
-
-
+bool isPerfectSquare(long long n) {
+    if ((int) (sqrt(n)) == sqrt(n)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-int main(){
+int main() {
+    long long input;
+    cout << "Enter a number: ";
+    cin >> input;
 
 
     return 0;
